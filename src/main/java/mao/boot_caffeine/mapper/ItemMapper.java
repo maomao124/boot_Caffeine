a18b47b46d28f7a3dd6d46b9ec3cdd14e97668e0
mao <1296193245@qq.com>
2022-06-24 17:56:05 +0800

    boot_Caffeine

--- src/main/java/mao/boot_caffeine/mapper/ItemMapper.java
+++ src/main/java/mao/boot_caffeine/mapper/ItemMapper.java
@@ -2,6 +2,7 @@ package mao.boot_caffeine.mapper;
 
 import mao.boot_caffeine.entity.Item;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author mao
  * @since 2022-06-24
  */
+
+@Mapper
 public interface ItemMapper extends BaseMapper<Item>
 {
 
